2bb9fe8e84e9a3ad2ed82f14b639e7d984c4a438
mohebifar <netfars@gmail.com>
2016-04-09 12:42:24 +0430

    feat(Storage): Add Storage class

---
+++ src/Storage.js
@@ -0,0 +1,67 @@
+import fs from 'react-native-fs';
+import md5 from 'blueimp-md5';
+
+const singleton = Symbol();
+const singletonEnforcer = Symbol();
+
+export default class Storage {
+  constructor(enforcer) {
+    if (enforcer !== singletonEnforcer) {
+      throw new Error('Cannot construct an object of Storage');
+    }
+
+    this.hashFunction = md5;
+    this.setCacheDirectory('images');
+  }
+
+  static get singleton() {
+    if (!this[singleton]) {
+      this[singleton] = new Storage(singletonEnforcer);
+    }
+
+    return this[singleton];
+  }
+
+  setCacheDirectory(directory) {
+    this.cacheDirectory = directory;
+    this.createCacheDirectory();
+  }
+
+  getCacheDirectory() {
+    return `${fs.DocumentDirectoryPath}/${this.cacheDirectory}`;
+  }
+
+  createCacheDirectory() {
+    fs.mkdir(this.getCacheDirectory());
+  }
+
+  hashUrl(url) {
+    return this.hashFunction(url);
+  }
+
+  getFilePath(url) {
+    return `${this.getCacheDirectory()}/${this.hashUrl(url)}`;
+  }
+
+  download(url) {
+    return fs.downloadFile(url, this.getFilePath(url));
+  }
+
+  load(url) {
+    const filePath = this.getFilePath(url);
+
+    return fs
+      .exists(filePath)
+      .then(exists => {
+        if (exists) {
+          return filePath;
+        } else {
+          return this
+            .download(url)
+            .then(() => filePath);
+        }
+      });
+  }
+}
+
+export const storage = Storage.singleton;
